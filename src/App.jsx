b89f569c77d6d3455b445d61d56a2390619421e9
PANKAJ BARMAN <pankaj@sublimedatasys.com>
2024-01-18 17:15:19 +0530

    [add] - icons and list items

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ import orangeIcon from "./assets/orangeIcon.svg";
 import grapesIcon from "./assets/grapesIcon.svg";
 import mangoIcon from "./assets/mangoIcon.svg";
 import blueberryIcon from "./assets/blueberryIcon.svg";
+import peachIcon from "./assets/peachIcon.svg";
+import papayaIcon from "./assets/papayaIcon.svg";
+import pomegranateIcon from "./assets/pomegranateIcon.svg";
+import lemonIcon from "./assets/lemonIcon.svg";
+import cherryIcon from "./assets/cherryIcon.svg";
+import pineappleIcon from "./assets/pineappleIcon.svg";
+import kiwiIcon from "./assets/kiwiIcon.svg";
+import watermelonIcon from "./assets/watermelonIcon.svg";
+import strawberryIcon from "./assets/strawberryIcon.svg";
+import avocadoIcon from "./assets/avocadoIcon.svg";
+import guavaIcon from "./assets/guavaIcon.svg";
 
 function App() {
   const [selectedItem, setSelectedItem] = useState("");
@@ -18,6 +29,17 @@ function App() {
       { icon: grapesIcon, text: "Grapes" },
       { icon: mangoIcon, text: "Mango" },
       { icon: blueberryIcon, text: "Blueberry" },
+      { icon: peachIcon, text: "Peach" },
+      { icon: papayaIcon, text: "Papaya" },
+      { icon: pomegranateIcon, text: "Pomegranate" },
+      { icon: lemonIcon, text: "Lemon" },
+      { icon: cherryIcon, text: "Cherry" },
+      { icon: pineappleIcon, text: "Pineapple" },
+      { icon: kiwiIcon, text: "Kiwi" },
+      { icon: watermelonIcon, text: "Watermelon" },
+      { icon: strawberryIcon, text: "Strawberry" },
+      { icon: avocadoIcon, text: "Avocado" },
+      { icon: guavaIcon, text: "Guava" },
     ],
     []
   );
